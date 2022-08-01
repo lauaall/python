b4eb7dba11b968c4069393c3454e9b916e94dcb2
lauaall <48836917+skelq@users.noreply.github.com>
2022-08-01 14:03:10 +0300

    added time and improvements

--- RemoveBak.py
+++ RemoveBak.py
@@ -4,6 +4,7 @@ import os
 def main():
     directory = input("Enter a directory: ")
     extension = input("Enter a file extension(without .): ")
+    print("")
     os.chdir(directory)
     files = os.listdir(directory)
     for file in files:
@@ -13,4 +14,11 @@ def main():
             print("Renamed {} to {}".format(file, new_file))
     print("")
     print("Done!")
+if __name__ == "__main__":
+    start_time = os.times()
+    main()
+    end_time = os.times()
+    print("Operation took {} seconds".format(end_time[0] - start_time[0]))
+    input("Press Enter to exit...")
+    exit()
 main()
